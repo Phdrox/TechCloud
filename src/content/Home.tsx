86d35c82610161a20da8ccc7c87d8b1b7cf43d4e
Phdrox <phximenes2103@gmail.com>
2024-10-04 18:38:33 -0300

    other upload

--- src/content/Home.tsx
+++ src/content/Home.tsx
@@ -7,6 +7,7 @@ import Projetos from './Projetos';
 import Tecnologia from './Tecnologia';
 import Sobre from './Sobre';
 import Contato from './Contato';
+import styles from '../../public/styles/home.module.css'
 
 export default function Home() {
   useEffect(()=>{AOS.init({duration:1000});},[])
@@ -15,7 +16,7 @@ export default function Home() {
   return (
     <div className='grid grid-rows-6 mobile:grid-cols-1 min-h-screen tablet:grid-cols-2 tablet:grid-rows-4' id='Home'>
      <Navbar/>
-     <div className='row-span-1 bg-[url("./image/Wallpaper.png")] mobile:gap-4 bg-no-repeat bg-cover flex items-center justify-center mobile:flex-col tablet:col-span-2 notebook:flex-row bg-fixed'>
+     <div className='row-span-1 mobile:gap-4 bg-no-repeat bg-cover flex items-center justify-center mobile:flex-col tablet:col-span-2 notebook:flex-row' id={styles.home}>
         <div className=' flex w-full mobile:order-2 mobile:gap-4 mobile:flex-col mobile:items-center notebook:order-1 notebook:items-start notebook:p-3 notebook:justify-between' data-aos='fade-left'>
           <h1 className='text-white tablet:text-2xl flex gap-1 flex-col tablet:items-center notebook:gap-0 notebook:items-start'><span className='font-bold text-xl tablet:text-3xl notebook:text-5xl'>Faça um upgrade</span> no seu negócio </h1>
           <p className='flex text-white text-xs  mobile:w-10/12 mobile:text-center notebook:w-10/12 notebook:text-left' >A TechCloud tem o prazer de apresentar sua expertise no desenvolvimento de sites e sistemas web, oferecemos soluções que atendem às necessidades exclusivas de cada cliente.</p>
